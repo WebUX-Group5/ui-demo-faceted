0a2a2a992db4e8b012b0d075401cc73495eb6c51
mf <mf@tt4.at>
2016-03-01 13:30:52 +0100

    Python client

--- src/proxy/server.py
+++ src/proxy/server.py
@@ -1,15 +1,21 @@
 from flask import Flask, request, jsonify
+from flask.ext.cors import CORS
 from elasticsearch import Elasticsearch
 import json
 
 app = Flask(__name__)
+app.debug=True
+CORS(app)
+
 client = Elasticsearch()
 
 @app.route('/search', methods=['POST'])
 def es():
     data = request.get_json(force=True)
-    body = dict([(k, data[k]) for k in ['query', 'aggregations'] if k in data])
-    result = client.search(body=data)
+    body = dict([(k, data['body'][k]) for k in ['query', 'aggregations']\
+                 if k in data['body']])
+    result = client.search(body=data['body'])
+    print(result)
     return jsonify(result)
 
 if __name__ == '__main__':
