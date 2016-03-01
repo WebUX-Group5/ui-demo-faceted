11f3f63552c518557bc90dd3f03b6202dd3e5a4e
mf <mf@tt4.at>
2016-03-01 16:10:21 +0100

    Fix

--- src/app/scripts/app.js
+++ src/app/scripts/app.js
@@ -15,7 +15,7 @@
 
   // esFactory() creates a configured client instance. Turn that instance
   // into a service so that it can be required by other parts of the application
-  app.service('esClientJs', ['esClientJs', function (esFactory) {
+  app.service('esClientJs', ['esFactory', function (esFactory) {
     return esFactory({
       host: 'localhost:9200/opendata',
       //apiVersion: '1.2',
