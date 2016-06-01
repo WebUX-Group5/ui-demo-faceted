33e7035945a14f0a1078a65a308bb1562394afb6
mf <mf@tt4.at>
2016-06-01 11:32:23 +0200

    Configuration, about-information.

--- src/app/scripts/config.js
+++ src/app/scripts/config.js
@@ -1,7 +1,15 @@
 (function () {
   'use strict';
   var app = angular.module('FacetedUI');
+
+  /**
+   * Configure to either use elasticsearch directly, or use a proxy
+   * in between.
+   * Set the 'esClient' option to either 'py' or 'js'.
+   */
   app.constant('Config', {
-    esClient: 'js'    // 'py' or 'js'
+    esClient: 'py',
+    pyEndpoint: '/search',
+    esEndpoint: '',
   });
 }());
